294460a0bfe218ea7f507e7dae527d7d1e0bcc4c
ZaraNazaretyan <zara.nazaretyan92@gmail.com>
2023-07-21 14:06:14 +0400

    first commit

---
+++ pepi_martin.js
@@ -0,0 +1,9 @@
+"use strict"
+let tikin = document.querySelector(".img_1");
+tikin.addEventListener("click", function(){
+    tikin.style.borderRadius = "50%"
+    tikin.style.transform = "scale(-1,1)";
+})
+
+
+
